683afd559750c0cf07bd4ec213aeba25b6d83b94
xBA5ED <0xBA5ED@protonmail.com>
2022-07-05 22:29:05 +0200

    Fix 'Identifier not found'

--- contracts/interfaces/IJBTieredLimitedNFTRewardDataSourceProjectDeployer.sol
+++ contracts/interfaces/IJBTieredLimitedNFTRewardDataSourceProjectDeployer.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.6;
 
+import '@jbx-protocol/contracts-v2/contracts/interfaces/IJBController.sol';
 import '@jbx-protocol/contracts-v2/contracts/structs/JBProjectMetadata.sol';
 import '../structs/JBDeployTieredNFTRewardDataSourceData.sol';
 import '../structs/JBLauchFundingCyclesData.sol';
