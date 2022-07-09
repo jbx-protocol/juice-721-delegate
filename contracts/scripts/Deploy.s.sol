bdea2d8f3d7e8304bc8aafc292605bd91e65a75d
DrGorilla_md <dr_gorilla@protonmail.com>
2022-07-09 22:43:53 +0200

    launch project script

--- contracts/scripts/Deploy.s.sol
+++ contracts/scripts/Deploy.s.sol
@@ -5,24 +5,22 @@ import 'forge-std/Script.sol';
 
 contract DeployMainnet is Script {
   IJBController jbController = IJBController(0x43780E780DF2bD1e4d955d3d4b577a490841241A);
-  
-  function setUp() public {
-  }
+
+  function setUp() public {}
 
   function run() external {
     vm.startBroadcast();
-   new JBTieredLimitedNFTRewardDataSourceProjectDeployer(jbController);
+    new JBTieredLimitedNFTRewardDataSourceProjectDeployer(jbController);
   }
 }
 
 contract DeployRinkeby is Script {
   IJBController jbController = IJBController(0xb51e584Ee10460E9e5ea29Deab67cd347B0cA2f3);
-  
-  function setUp() public {
-  }
+
+  function setUp() public {}
 
   function run() external {
     vm.startBroadcast();
-   new JBTieredLimitedNFTRewardDataSourceProjectDeployer(jbController);
+    new JBTieredLimitedNFTRewardDataSourceProjectDeployer(jbController);
   }
 }
