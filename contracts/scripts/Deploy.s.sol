08782bfdabaec07f1e28a496ef26153b17018db0
mejango <mejango@protonmail.com>
2022-07-31 19:56:46 -0400

    moved to library

--- contracts/scripts/Deploy.s.sol
+++ contracts/scripts/Deploy.s.sol
@@ -7,11 +7,16 @@ contract DeployMainnet is Script {
   IJBController jbController = IJBController(0x43780E780DF2bD1e4d955d3d4b577a490841241A);
   IJBOperatorStore jbOperatorStore = IJBOperatorStore(0x6F3C5afCa0c9eDf3926eF2dDF17c8ae6391afEfb);
 
+  JBTieredLimitedNFTRewardDataSourceProjectDeployer jbTieredLimitedNFTRewardDataSource;
+
   function setUp() public {}
 
   function run() external {
     vm.startBroadcast();
-    new JBTieredLimitedNFTRewardDataSourceProjectDeployer(jbController, jbOperatorStore);
+    jbTieredLimitedNFTRewardDataSource = new JBTieredLimitedNFTRewardDataSourceProjectDeployer(
+      jbController,
+      jbOperatorStore
+    );
   }
 }
 
@@ -19,10 +24,15 @@ contract DeployRinkeby is Script {
   IJBController jbController = IJBController(0xb51e584Ee10460E9e5ea29Deab67cd347B0cA2f3);
   IJBOperatorStore jbOperatorStore = IJBOperatorStore(0xEDB2db4b82A4D4956C3B4aA474F7ddf3Ac73c5AB);
 
+  JBTieredLimitedNFTRewardDataSourceProjectDeployer jbTieredLimitedNFTRewardDataSource;
+
   function setUp() public {}
 
   function run() external {
     vm.startBroadcast();
-    new JBTieredLimitedNFTRewardDataSourceProjectDeployer(jbController, jbOperatorStore);
+    jbTieredLimitedNFTRewardDataSource = new JBTieredLimitedNFTRewardDataSourceProjectDeployer(
+      jbController,
+      jbOperatorStore
+    );
   }
 }
