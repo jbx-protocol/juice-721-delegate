2896b4d4c07fa74b1e984592f10ab51abf595e0b
DrGorilla_md <dr_gorilla@protonmail.com>
2022-11-26 23:52:38 +0100

    feat: OZ clone

--- contracts/JBTiered721DelegateDeployer.sol
+++ contracts/JBTiered721DelegateDeployer.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
+import '@openzeppelin/contracts/proxy/Clones.sol';
+
 import './interfaces/IJBTiered721DelegateDeployer.sol';
 import './JBTiered721Delegate.sol';
 import './JB721TieredGovernance.sol';
@@ -16,8 +18,6 @@ import './JB721GlobalGovernance.sol';
 */
 contract JBTiered721DelegateDeployer is IJBTiered721DelegateDeployer {
   error INVALID_GOVERNANCE_TYPE();
-
-  uint256 constant DEPLOY_BYTECODE_LENGTH = 13;
 
   //*********************************************************************//
   // --------------- public immutable stored properties ---------------- //
@@ -73,16 +73,14 @@ contract JBTiered721DelegateDeployer is IJBTiered721DelegateDeployer {
     JBDeployTiered721DelegateData memory _deployTiered721DelegateData
   ) external override returns (IJBTiered721Delegate newDelegate) {
     // Deploy the governance variant that was requested
-    address codeToCopy;
     if (_deployTiered721DelegateData.governanceType == JB721GovernanceType.NONE)
-      codeToCopy = address(noGovernance);
+      newDelegate = IJBTiered721Delegate(Clones.clone(address(noGovernance)));
     else if (_deployTiered721DelegateData.governanceType == JB721GovernanceType.TIERED)
-      codeToCopy = address(tieredGovernance);
+      newDelegate = IJBTiered721Delegate(Clones.clone(address(tieredGovernance)));
     else if (_deployTiered721DelegateData.governanceType == JB721GovernanceType.GLOBAL)
-      codeToCopy = address(globalGovernance);
+      newDelegate = IJBTiered721Delegate(Clones.clone(address(globalGovernance)));
     else revert INVALID_GOVERNANCE_TYPE();
 
-    newDelegate = IJBTiered721Delegate(_clone(codeToCopy));
     newDelegate.initialize(
       _projectId,
       _deployTiered721DelegateData.directory,
@@ -104,39 +102,5 @@ contract JBTiered721DelegateDeployer is IJBTiered721DelegateDeployer {
     emit DelegateDeployed(_projectId, newDelegate, _deployTiered721DelegateData.governanceType);
 
     return newDelegate;
-  }
-
-  /**
-    @notice Clone and redeploy the bytecode of a given address
-
-    @dev Runtime bytecode needs a constructor -> we append this one
-         to the bytecode, which is a minimalistic one only returning the runtime bytecode
-
-         See https://github.com/drgorillamd/clone-deployed-contract/blob/master/readme.MD for details
-   */
-  function _clone(address _targetAddress) internal returns (address _out) {
-    assembly {
-      // Get deployed/runtime code size
-      let _codeSize := extcodesize(_targetAddress)
-
-      // Get a bit of freemem to land the bytecode, not updated as we'll leave this scope right after create(..)
-      let _freeMem := mload(0x40)
-
-      // Shift the length to the length placeholder, in the constructor (by adding zero's/mul)
-      let _mask := mul(_codeSize, 0x100000000000000000000000000000000000000000000000000000000)
-
-      // Insert the length in the correct spot (after the PUSH3 / 0x62)
-      let _initCode := or(_mask, 0x62000000600081600d8239f3fe00000000000000000000000000000000000000)
-      // --------------------------- here ^ (see the "1" from the mul step aligning)
-
-      // Store the deployment bytecode in free memory
-      mstore(_freeMem, _initCode)
-
-      // Copy the bytecode, after the deployer bytecode in free memory
-      extcodecopy(_targetAddress, add(_freeMem, DEPLOY_BYTECODE_LENGTH), 0, _codeSize)
-
-      // Deploy the copied bytecode (constructor + original) and return the address in 'out'
-      _out := create(0, _freeMem, add(_codeSize, DEPLOY_BYTECODE_LENGTH))
-    }
   }
 }
