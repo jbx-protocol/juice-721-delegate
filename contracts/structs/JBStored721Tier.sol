2083800410aca4c92d1a79c6cec9e49df193f5f1
mejango <mejango@protonmail.com>
2023-03-25 23:53:11 -0700

    -

--- contracts/structs/JBStored721Tier.sol
+++ contracts/structs/JBStored721Tier.sol
@@ -5,8 +5,8 @@ pragma solidity ^0.8.0;
   @member price The minimum contribution to qualify for this tier.
   @member remainingQuantity Remaining number of tokens in this tier. Together with idCeiling this enables for consecutive, increasing token ids to be issued to contributors.
   @member initialQuantity The initial `remainingAllowance` value when the tier was set.
-  @member lockedUntil The time up to which this tier cannot be removed. The application interprets this value as days added to the timestamp for 1672531200 (Jan 1, 2023 00:00 UTC), allowing for storage in 24 bits. 
   @member votingUnits The amount of voting significance to give this tier compared to others.
+  @member lockedUntil The time up to which this tier cannot be removed. The application interprets this value as days added to the timestamp for 1672531200 (Jan 1, 2023 00:00 UTC), allowing for storage in 24 bits. 
   @member reservedRate The number of minted tokens needed in the tier to allow for minting another reserved token.
   @member category A category to group NFT tiers by.
   @member royaltyRate The percentage of each of the NFT sales that should be routed to the royalty beneficiary. Out of MAX_ROYALTY_RATE.
