6324440379bc2b4b56275278b756bd14c6811157
mejango <mejango@protonmail.com>
2023-05-09 18:05:56 -0700

    fidelity adjust

--- contracts/structs/JB721TierParams.sol
+++ contracts/structs/JB721TierParams.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
   @member price The minimum contribution to qualify for this tier.
   @member initialQuantity The initial `remainingAllowance` value when the tier was set.
   @member votingUnits The amount of voting significance to give this tier compared to others.
-  @memver reservedRate The number of minted tokens needed in the tier to allow for minting another reserved token.
+  @member reservedRate The number of minted tokens needed in the tier to allow for minting another reserved token.
   @member reservedRateBeneficiary The beneificary of the reserved tokens for this tier.
   @member encodedIPFSUri The URI to use for each token within the tier.
   @member category A category to group NFT tiers by.
